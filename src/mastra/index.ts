33a7c57becb7fdc5201543f305b634c6ac7a80a3
Soner Vergon <soner@vergon.se>
2025-02-23 17:10:08 +0100

    remove unused file

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,5 +1,5 @@
 import { Mastra } from "@mastra/core";
-import { myWorkflow } from "../workflow";
+import { myWorkflow } from "./workflow";
 
 export const mastra = new Mastra({
   workflows: { myWorkflow },
